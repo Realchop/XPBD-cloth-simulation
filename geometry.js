062d268bfa30c057ce66079dfd03408b313c5a36
Realchop <jovanovic.lazar002@gmail.com>
2024-04-15 23:56:03 +0200

    POC rotacija

---
+++ geometry.js
@@ -0,0 +1,218 @@
+"use strict";
+
+
+export class Geometry {
+    matrixMul(a, b) {
+        const aNumRows = a.length;
+        const aNumCols = a[0].length;
+
+        const bNumRows = b.length;
+        const bNumCols = b[0].length;
+    
+        let m = new Array(aNumRows);
+        for (let r = 0; r < aNumRows; ++r) 
+        {
+            m[r] = new Array(bNumCols); 
+        }
+
+        for (let r = 0; r < aNumRows; ++r) 
+        {
+            for (let c = 0; c < bNumCols; ++c) 
+            {
+                m[r][c] = 0;
+                for (let i = 0; i < aNumCols; ++i) 
+                {
+                    m[r][c] += a[r][i] * b[i][c];
+                }
+            }
+        }
+    
+        return m;
+    }
+
+    sinCache = {};
+    sin(deg) {
+        if(!this.sinCache[deg])
+            this.sinCache[deg] = Math.sin(deg);
+
+        return this.sinCache[deg];
+    }
+
+    cosCache = {};
+    cos(deg) {
+        if(!this.cosCache[deg])
+            this.cosCache[deg] = Math.cos(deg);
+
+        return this.cosCache[deg];
+    }
+}
+
+// Ovo je jako cringe nacin da se ovo radi al ajde
+const geometrySingleton = new Geometry();
+
+export class Point {
+    x; // X coordinate
+    y; // Y coordinate
+    z; // Z coordinate
+    r; // point radius
+    g = -9.81; // gravity 
+    up = null; // above neighbour
+    down = null; // below neighbour
+    left = null; // left neighbour
+    right = null; // right neighbour
+    ctx; // canvas 2d drawing context
+    drawSelf; // if points should be drawn
+
+    constructor(x, y, r, ctx) {
+        this.x = x;
+        this.y = y;
+        this.z = 0;
+        this.r = r;
+        this.ctx = ctx;
+        this.drawSelf = false;
+    }
+
+    updateSelf() {
+        // Ovde ide fizika recimo:
+        // this.y -= this.g;
+    }
+
+    rotateX(deg) {
+        let rotationMatrix = [
+            [1, 0, 0, 0],
+            [0, geometrySingleton.cos(deg), -geometrySingleton.sin(deg), 0],
+            [0, geometrySingleton.sin(deg), geometrySingleton.cos(deg), 0],
+            [0, 0, 0, 1]
+        ]
+
+        let t_1 = [
+            [ 1, 0, 0, window.innerWidth/2],
+            [ 0, 1, 0, window.innerHeight/2],
+            [ 0, 0, 1, 0],
+            [ 0, 0, 0, 1],
+        ]
+
+        let t = [
+            [ 1, 0, 0, -window.innerWidth/2],
+            [ 0, 1, 0, -window.innerHeight/2],
+            [ 0, 0, 1, 0],
+            [ 0, 0, 0, 1],
+        ]
+
+        let current = [
+            [this.x],
+            [this.y],
+            [this.z],
+            [1]
+        ]
+
+        let res1 = geometrySingleton.matrixMul(t_1, rotationMatrix);
+        let res2 = geometrySingleton.matrixMul(res1, t);
+        let res3 = geometrySingleton.matrixMul(res2, current);
+        this.x = res3[0][0];
+        this.y = res3[1][0];
+        this.z = res3[2][0];
+    }
+
+    rotateY(deg) {
+        let rotationMatrix = [
+            [geometrySingleton.cos(deg), 0, geometrySingleton.sin(deg), 0],
+            [0, 1, 0, 0],
+            [-geometrySingleton.sin(deg), 0, geometrySingleton.cos(deg), 0],
+            [0, 0, 0, 1]
+        ]
+
+        let t_1 = [
+            [ 1, 0, 0, window.innerWidth/2],
+            [ 0, 1, 0, window.innerHeight/2],
+            [ 0, 0, 1, 0],
+            [ 0, 0, 0, 1],
+        ]
+
+        let t = [
+            [ 1, 0, 0, -window.innerWidth/2],
+            [ 0, 1, 0, -window.innerHeight/2],
+            [ 0, 0, 1, 0],
+            [ 0, 0, 0, 1],
+        ]
+
+        let current = [
+            [this.x],
+            [this.y],
+            [this.z],
+            [1]
+        ]
+
+        let res1 = geometrySingleton.matrixMul(t_1, rotationMatrix);
+        let res2 = geometrySingleton.matrixMul(res1, t);
+        let res3 = geometrySingleton.matrixMul(res2, current);
+        this.x = res3[0][0];
+        this.y = res3[1][0];
+        this.z = res3[2][0];
+    }
+
+    rotateZ(deg) {
+        let t_1 = [
+            [ 1, 0, 0, window.innerWidth/2],
+            [ 0, 1, 0, window.innerHeight/2],
+            [ 0, 0, 1, 0],
+            [ 0, 0, 0, 1],
+        ]
+
+        let t = [
+            [ 1, 0, 0, -window.innerWidth/2],
+            [ 0, 1, 0, -window.innerHeight/2],
+            [ 0, 0, 1, 0],
+            [ 0, 0, 0, 1],
+        ]
+
+        let rotationMatrix = [
+            [geometrySingleton.cos(deg), -geometrySingleton.sin(deg), 0, 0],
+            [geometrySingleton.sin(deg), geometrySingleton.cos(deg), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+
+        let current = [
+            [this.x],
+            [this.y],
+            [this.z],
+            [1]
+        ]
+
+        let res1 = geometrySingleton.matrixMul(t_1, rotationMatrix);
+        let res2 = geometrySingleton.matrixMul(res1, t);
+        let res3 = geometrySingleton.matrixMul(res2, current);
+        this.x = res3[0][0];
+        this.y = res3[1][0];
+        this.z = res3[2][0];
+    }
+
+    draw(overrideDrawSelf=false) {
+        this.updateSelf();
+        if(overrideDrawSelf || this.drawSelf)
+        {
+            // Posto ne koristimo z, to je implicitna projekcija
+            // na ravan Oxy.
+            this.ctx.beginPath();
+            this.ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+            this.ctx.fill();
+        }
+
+        if(this.up !== null)
+        {
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.x, this.y);
+            this.ctx.lineTo(this.up.x, this.up.y);
+            this.ctx.stroke();
+        }
+
+        if(this.left !== null)
+        {
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.x, this.y);
+            this.ctx.lineTo(this.left.x, this.left.y);
+            this.ctx.stroke();
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,8 +1,98 @@
-import * as THREE from 'three';
+"use strict";
+import { Point } from "./geometry";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// Canvas setup
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const numberOfPoints = 30;
+const r = 2;
+const clothSide = 500;
+const distanceCoef = clothSide/numberOfPoints;
+const startX = (window.innerWidth-(distanceCoef*(numberOfPoints-1)))/2;
+const startY = (window.innerHeight-(distanceCoef*(numberOfPoints-1)))/2;
+
+let points = [];
+
+for(let i=0; i<numberOfPoints; ++i)
+{
+    points.push([]);
+    for(let j=0; j<numberOfPoints; ++j)
+    {
+        points[i].push(new Point(startX+distanceCoef*i, startY+distanceCoef*j, r, ctx));
+    }
+}
+
+for(let i=0; i<numberOfPoints; ++i)
+{
+    for(let j=0; j<numberOfPoints; ++j)
+    {
+        if(i > 0)
+        {
+            points[i][j].left = points[i-1][j];
+        }
+        if(j > 0)
+        {
+            points[i][j].up = points[i][j-1];
+        }
+    }
+}
+
+let paused = false;
+let showScreenCenter = true;
+let drawPoints = false;
+// *************************************************** //
+//                     COPY PASTA                      //
+let framesPerSecond = 60;
+let interval = Math.floor(1000 / framesPerSecond);
+let startTime = performance.now();
+let previousTime = startTime;
+let currentTime = 0;
+let deltaTime = 0;
+//                       END                          //
+// ************************************************** //
+
+// Diskutuj umesto ovog realtime pristupa pre render pristup
+function animate(timestamp)
+{
+    if(paused) return;
+
+    window.requestAnimationFrame(animate);
+
+    currentTime = timestamp;
+    deltaTime = currentTime - previousTime;
+  
+    if (deltaTime > interval) {
+      previousTime = currentTime - (deltaTime % interval);
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      for(const row of points)
+      {
+          for(const point of row)
+          {
+            point.rotateX(Math.PI/60);
+            point.rotateY(Math.PI/60);
+            point.rotateZ(Math.PI/60);
+            point.draw(drawPoints);
+          }
+      }
+
+    }
+
+    // Dodatan info
+    if(showScreenCenter)
+    {
+        ctx.beginPath();
+        ctx.arc(window.innerWidth/2, window.innerHeight/2, 3, 0, Math.PI*2);
+        ctx.fill();
+    }
+}
+animate();
+
+document.body.addEventListener('click', () => {
+    paused = !paused;
+    if(!paused) animate();
+})
