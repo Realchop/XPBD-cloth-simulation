3e0c9fb78fde7f23cf20b4d03ddaa5e59a3186b2
Realchop <jovanovic.lazar002@gmail.com>
2024-05-12 20:05:42 +0200

    Inital xpbd

---
+++ xpbd/geometry.js
@@ -0,0 +1,352 @@
+"use strict";
+
+
+export class Geometry 
+{
+    matrixMul(a, b) {
+        const aNumRows = a.length;
+        const aNumCols = a[0].length;
+
+        const bNumRows = b.length;
+        const bNumCols = b[0].length;
+    
+        let m = new Array(aNumRows);
+        for (let r = 0; r < aNumRows; ++r) 
+        {
+            m[r] = new Array(bNumCols); 
+        }
+
+        for (let r = 0; r < aNumRows; ++r) 
+        {
+            for (let c = 0; c < bNumCols; ++c) 
+            {
+                m[r][c] = 0;
+                for (let i = 0; i < aNumCols; ++i) 
+                {
+                    m[r][c] += a[r][i] * b[i][c];
+                }
+            }
+        }
+    
+        return m;
+    }
+
+    sinCache = {};
+    sin(deg) {
+        if(!this.sinCache[deg])
+            this.sinCache[deg] = Math.sin(deg);
+
+        return this.sinCache[deg];
+    }
+
+    cosCache = {};
+    cos(deg) {
+        if(!this.cosCache[deg])
+            this.cosCache[deg] = Math.cos(deg);
+
+        return this.cosCache[deg];
+    }
+
+    rotations = new Map();
+
+    initialize () {
+        this.rotations.set(Math.PI, null);
+        this.rotations.set(Math.PI / 2, null);
+        this.rotations.set(Math.PI / 3, null);
+        this.rotations.set(Math.PI / 4, null);
+        this.rotations.set(Math.PI / 6, null);
+        this.rotations.set(Math.PI / 18, null);
+
+        let t_1 = [
+            [ 1, 0, 0, window.innerWidth/2],
+            [ 0, 1, 0, window.innerHeight/2],
+            [ 0, 0, 1, 0],
+            [ 0, 0, 0, 1],
+        ];
+
+        let t = [
+            [ 1, 0, 0, -window.innerWidth/2],
+            [ 0, 1, 0, -window.innerHeight/2],
+            [ 0, 0, 1, 0],
+            [ 0, 0, 0, 1],
+        ];
+
+        let pos = [
+            [[1, 1], [1, 2], [2, 1], [2, 2]],
+            [[0, 0], [2, 0], [0, 2], [2, 2]],
+            [[0, 0], [0, 1], [1, 0], [1, 1]]
+        ]
+        for (let key of this.rotations.keys()) {
+            let axis = {
+                x: null,
+                y: null,
+                z: null
+            }
+            for (let i=0; i<3; ++i) {
+                let p = pos[i];
+                let e = [
+                    [1, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]
+                ]
+                e[p[0][0]][p[0][1]] = this.cos(key);
+                e[p[1][0]][p[1][1]] = -this.sin(key);
+                e[p[2][0]][p[2][1]] = this.sin(key);
+                e[p[3][0]][p[3][1]] = this.cos(key);
+
+                e = this.matrixMul (t_1, e);
+                e = this.matrixMul (e, t);
+                switch (i) {
+                    case 0:
+                        axis.x = e;
+                        break;
+                    case 1:
+                        axis.y = e;
+                        break;
+                    case 2:
+                        axis.z = e;
+                }
+            }
+            this.rotations.set(key, axis);
+        }
+    }
+
+    angleReduce (deg, axis) {
+        let degCopy = deg;
+        let comp = [];
+        let current = 0;
+        let angles = [Math.PI, Math.PI / 2, Math.PI / 3, Math.PI / 4, Math.PI / 6, Math.PI / 18];
+        while (deg - (Math.PI / 18) >= 0) {
+            if (deg - angles[current] >= 0) {
+                comp.push (this.rotations.get(angles[current])[axis]);
+                deg -= angles[current];
+            } else current++;
+        }
+        let res = comp[0];
+        for (let i=1; i<comp.length; ++i) res = this.matrixMul (res, comp[i]);
+        this.rotations.get(degCopy)[axis] = res;
+    }
+
+    constructor () {
+        this.initialize();
+    }
+
+}
+
+// Ovo je jako cringe nacin da se ovo radi al ajde
+const geometrySingleton = new Geometry();
+
+export class Point 
+{
+    x; // X coordinate
+    y; // Y coordinate
+    z; // Z coordinate
+    r; // point radius
+    up = null; // above neighbour
+    down = null; // below neighbour
+    left = null; // left neighbour
+    right = null; // right neighbour
+    ctx; // canvas 2d drawing context (canvas je html element, ctx je vezan za canvas)
+    drawSelf; // if points should be drawn (bool)
+    distanceCoef;
+    // Novo
+    previous;
+    velocity;
+    w; // inverse mass
+
+    constructor(x, y, r, ctx, distanceCoef)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = 0.0;
+        this.r = r;
+        this.ctx = ctx;
+        this.drawSelf = false;
+        this.distanceCoef = distanceCoef;
+        // Novo
+        this.previous = [x, y, 0.0];
+        this.velocity = {};
+        this.velocity.x = 0.0;
+        this.velocity.y = 0.0;
+        this.velocity.z = 0.0;
+        this.w = 1.0;
+    }
+    
+    tiedNode() 
+    {
+        return this.up == null && 
+            (
+                this.left == null || 
+                this.right == null //|| 
+                // this.left.left == null || 
+                // this.right.right == null
+            )
+    }
+
+    distance(A)
+    {
+        return Math.sqrt((this.x - A.x) * (this.x - A.x) + 
+                         (this.y - A.y) * (this.y - A.y) + 
+                         (this.z - A.z) * (this.z - A.z));
+    }
+
+    rotateX(deg) 
+    {
+        if(deg < 0 || deg > Math.PI * 2)
+            deg = (deg % (2 * Math.PI) + 2 * Math.PI) % (2 * Math.PI);
+        if (!geometrySingleton.rotations.has(deg)) {
+            geometrySingleton.rotations.set(deg, {
+                x: null,
+                y: null,
+                z: null
+            });
+        }
+        if (!geometrySingleton.rotations.get(deg)["x"]) {
+            geometrySingleton.angleReduce(deg, "x");
+        }
+
+        let rotationMatrix = geometrySingleton.rotations.get(deg)["x"];
+        let current = [
+            [this.x],
+            [this.y],
+            [this.z],
+            [1]
+        ];
+
+        let res = geometrySingleton.matrixMul(rotationMatrix, current);
+        this.x = res[0][0];
+        this.y = res[1][0];
+        this.z = res[2][0];
+    }
+
+    rotateY(deg) 
+    {
+        if(deg < 0 || deg > Math.PI * 2)
+            deg = (deg % (2 * Math.PI) + 2 * Math.PI) % (2 * Math.PI);
+        if (!geometrySingleton.rotations.has(deg)) {
+            geometrySingleton.rotations.set(deg, {
+                x: null,
+                y: null,
+                z: null
+            })
+        }
+        if (!geometrySingleton.rotations.get(deg)["y"]) {
+            geometrySingleton.angleReduce(deg, "y");
+        }
+
+        let rotationMatrix = geometrySingleton.rotations.get(deg).y;
+        let current = [
+            [this.x],
+            [this.y],
+            [this.z],
+            [1]
+        ]
+
+        let res = geometrySingleton.matrixMul(rotationMatrix, current);
+        this.x = res[0][0];
+        this.y = res[1][0];
+        this.z = res[2][0];
+    }
+
+    rotateZ(deg) 
+    {
+        if(deg < 0 || deg > Math.PI * 2)
+            deg = (deg % (2 * Math.PI) + 2 * Math.PI) % (2 * Math.PI);
+        if (!geometrySingleton.rotations.has(deg)) {
+            geometrySingleton.rotations.set(deg, {
+                x: null,
+                y: null,
+                z: null
+            })
+        }
+        if (!geometrySingleton.rotations.get(deg)["z"]) {
+            geometrySingleton.angleReduce(deg, "z");
+        }
+
+        let rotationMatrix = geometrySingleton.rotations.get(deg).z;
+        let current = [
+            [this.x],
+            [this.y],
+            [this.z],
+            [1]
+        ]
+
+        let res = geometrySingleton.matrixMul(rotationMatrix, current);
+        this.x = res[0][0];
+        this.y = res[1][0];
+        this.z = res[2][0];
+    }
+
+    draw(overrideDrawSelf=false, drawConstraints=false) 
+    {
+        if(overrideDrawSelf || this.drawSelf)
+        {
+            // Posto ne koristimo z, to je implicitna projekcija
+            // na ravan Oxy.
+            this.ctx.beginPath();
+            this.ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+            this.ctx.fill();
+        }
+
+        if(this.up !== null)// && (this.left === null || this.right === null))
+        {
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.x, this.y);
+            this.ctx.lineTo(this.up.x, this.up.y);
+            this.ctx.stroke();
+        }
+
+        if(this.left !== null)// && (this.up === null || this.down === null))
+        {
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.x, this.y);
+            this.ctx.lineTo(this.left.x, this.left.y);
+            this.ctx.stroke();
+        }
+
+        // Constraints
+        if(drawConstraints && this.down != null) 
+        {
+            // trougao
+            if(this.down.left != null)
+            {
+                this.ctx.beginPath();
+                this.ctx.moveTo(this.x, this.y);
+                this.ctx.lineTo(this.down.left.x, this.down.left.y);
+                this.ctx.stroke();
+            }
+            // crevno
+            if(this.down.right != null)
+            {
+                this.ctx.beginPath();
+                this.ctx.moveTo(this.x, this.y);
+                this.ctx.lineTo(this.down.right.x, this.down.right.y);
+                this.ctx.strokeStyle = "red";
+                this.ctx.stroke();
+                this.ctx.strokeStyle = "black";
+            }
+            // zeleno
+            if(this.down.down != null && this.down.down.left != null)
+            {
+                this.ctx.beginPath();
+                this.ctx.moveTo(this.x, this.y);
+                this.ctx.lineTo(this.down.down.left.x, this.down.down.left.y);
+                this.ctx.strokeStyle = "green";
+                this.ctx.stroke();
+                this.ctx.strokeStyle = "black";
+            }
+            // zuto
+            if(this.down.left != null && this.down.left.left != null)
+            {
+                this.ctx.beginPath();
+                this.ctx.moveTo(this.x, this.y);
+                this.ctx.lineTo(this.down.left.left.x, this.down.left.left.y);
+                this.ctx.strokeStyle = "yellow";
+                this.ctx.stroke();
+                this.ctx.strokeStyle = "black";
+            }
+        }
+    }
+
+}
---
+++ xpbd/main.js
@@ -0,0 +1,359 @@
+"use strict";
+import { Point } from "./geometry";
+
+
+// Canvas setup
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth; 
+canvas.height = window.innerHeight;
+
+const numberOfPoints = 40;
+const r = 2; 
+const clothSide = 500;
+const distanceCoef = clothSide/numberOfPoints;
+const startX = (window.innerWidth-(distanceCoef*(numberOfPoints-1)))/2;
+const startY = (window.innerHeight-(distanceCoef*(numberOfPoints-1)))/2;
+
+// Inicijalizacija tacaka
+let points = [];
+for(let i=0; i<numberOfPoints; ++i)
+{
+    points.push([]);
+    for(let j=0; j<numberOfPoints; ++j)
+    {
+        points[i].push(new Point(startX+distanceCoef*j, startY+distanceCoef*i, r, ctx, distanceCoef));
+    }
+}
+
+// Povezivanje
+for(let i=0; i<numberOfPoints; ++i)
+{
+    for(let j=0; j<numberOfPoints; ++j)
+    {
+        if(j > 0)
+        {
+            points[i][j].left = points[i][j-1];
+        }
+        if(i > 0)
+        {
+            points[i][j].up = points[i-1][j];
+        }
+        if (j < numberOfPoints-1)
+        {
+            points[i][j].right = points[i][j+1];
+        }
+        if (i < numberOfPoints-1)
+        {
+            points[i][j].down = points[i+1][j];
+        }
+    }
+}
+
+// Vezani cvorovi
+for(let i=0; i<numberOfPoints; ++i)
+{
+    for(let j=0; j<numberOfPoints; ++j)
+    {
+        if(points[i][j].tiedNode()) 
+        {
+            points[i][j].w = 0;
+            // console.log(i, j);
+        }
+    }
+}
+
+let paused = false;
+let showScreenCenter = false;
+let drawPoints = false;
+let framesPerSecond = 30;
+let startTime = performance.now();
+let previousTime = startTime;
+let currentTime = 0;
+let deltaTime = 0;
+
+// Parametri
+const steps = 30;
+const dt = 1000.0 / framesPerSecond;
+
+// External forces
+const g = [0, 9.81/1000, 0];
+const wind = [0.0, 9.81/1000, 7.4/1000];
+
+function xpbd()
+{
+    let sdt = dt / steps;
+    for(let step=0; step < steps; ++step)
+    {
+        preSolve(sdt, wind);
+        solve(sdt);
+        postSolve(sdt);
+    }
+}
+
+function preSolve(dt, wind)
+{
+    for(let i=0; i < numberOfPoints; ++i)
+    {
+        for(let j=0; j < numberOfPoints; ++j)
+        {
+            if(points[i][j].w == 0) continue;
+            // vec add
+            points[i][j].velocity.x += wind[0] * dt; 
+            points[i][j].velocity.y += wind[1] * dt; 
+            points[i][j].velocity.z += wind[2] * dt; 
+            
+            // vec copy
+            points[i][j].previous = [points[i][j].x, points[i][j].y, points[i][j].z];
+    
+            // vec add
+            points[i][j].x += points[i][j].velocity.x * dt; 
+            points[i][j].y += points[i][j].velocity.y * dt; 
+            points[i][j].z += points[i][j].velocity.z * dt; 
+        }
+    }
+}
+
+function solve(dt) 
+{
+    solveStretching(dt);
+    solveBending(dt);
+};
+
+let stretchingEdges = [];
+let bendingEdges = [];
+for(let i=0; i<numberOfPoints; ++i)
+{
+    for(let j=0; j<numberOfPoints; ++j)
+    {
+        if(points[i][j].down != null)
+        {            
+            stretchingEdges.push({
+            first:points[i][j], 
+            second:points[i][j].down, 
+            restingLen: points[i][j].distance(points[i][j].down)});
+            
+            // crevno
+            if(points[i][j].down.right != null)
+            {
+                bendingEdges.push({
+                    first:points[i][j], 
+                    second:points[i][j].down.right, 
+                    restingLen: points[i][j].distance(points[i][j].down.right)});
+            }
+            // zeleno
+            if(points[i][j].down.down != null && points[i][j].down.down.left != null)
+            {
+                bendingEdges.push({
+                    first:points[i][j], 
+                    second:points[i][j].down.down.left, 
+                    restingLen: points[i][j].distance(points[i][j].down.down.left)});
+            }
+            // zuto
+            if(points[i][j].down.left != null && points[i][j].down.left.left != null)
+            {
+                bendingEdges.push({
+                    first:points[i][j], 
+                    second:points[i][j].down.left.left, 
+                    restingLen: points[i][j].distance(points[i][j].down.left.left)});
+            }
+        }
+        if(points[i][j].right != null)
+        {
+            stretchingEdges.push({
+                first:points[i][j], 
+                second:points[i][j].right, 
+                restingLen: points[i][j].distance(points[i][j].right)});
+        }
+    }
+}
+
+function solveStretching(dt) 
+{
+    const stretchCoef = 1.05;
+    const alpha = stretchCoef / dt / dt;
+    const loggingIndex = -1;
+    for(let i=0; i<stretchingEdges.length; ++i)
+    {
+        let firstPoint = stretchingEdges[i].first;
+        let secondPoint = stretchingEdges[i].second;
+        const w = firstPoint.w + secondPoint.w;
+        if(w == 0.0) continue;
+        let edgeLength = firstPoint.distance(secondPoint);
+        if(edgeLength == 0.0) continue;
+        let vektorPravca = [
+            firstPoint.x - secondPoint.x, 
+            firstPoint.y - secondPoint.y,
+            firstPoint.z - secondPoint.z
+        ];
+
+        for(let j=0; j<3; ++j) vektorPravca[j] /= edgeLength;
+
+        if(i == loggingIndex)
+            console.log(vektorPravca);
+        const restingLen = stretchingEdges[i].restingLen;
+        let C = edgeLength - restingLen;
+        let s = -C / (w + alpha);
+        // vecAdd(this.pos,id0, this.grads,0, s * w0);
+        firstPoint.x += vektorPravca[0] * s * firstPoint.w;
+        firstPoint.y += vektorPravca[1] * s * firstPoint.w;
+        firstPoint.z += vektorPravca[2] * s * firstPoint.w;
+        // vecAdd(this.pos,id1, this.grads,0, -s * w1);
+        secondPoint.x -= vektorPravca[0] * s * secondPoint.w;
+        secondPoint.y -= vektorPravca[1] * s * secondPoint.w;
+        secondPoint.z -= vektorPravca[2] * s * secondPoint.w;
+
+        if(i == loggingIndex)
+            console.log(stretchingEdges[i].first.x, stretchingEdges[i].first.y, stretchingEdges[i].first.z, stretchingEdges[i].first.velocity);
+    }
+};
+
+function solveBending(dt)
+{
+    const bendingCoef = 10.0;
+    const alpha = bendingCoef / dt / dt;
+    const loggingIndex = -1;
+    for(let i=0; i<bendingEdges.length; ++i)
+    {
+        let firstPoint = bendingEdges[i].first;
+        let secondPoint = bendingEdges[i].second;
+        const w = firstPoint.w + secondPoint.w;
+        if(w == 0.0) continue;
+        let edgeLength = firstPoint.distance(secondPoint);
+        if(edgeLength == 0.0) continue;
+        let vektorPravca = [
+            firstPoint.x - secondPoint.x, 
+            firstPoint.y - secondPoint.y,
+            firstPoint.z - secondPoint.z
+        ];
+
+        for(let j=0; j<3; ++j) vektorPravca[j] /= edgeLength;
+
+        if(i == loggingIndex)
+            console.log(vektorPravca);
+        const restingLen = bendingEdges[i].restingLen;
+        let C = edgeLength - restingLen;
+        let s = -C / (w + alpha);
+        // vecAdd(this.pos,id0, this.grads,0, s * w0);
+        firstPoint.x += vektorPravca[0] * s * firstPoint.w;
+        firstPoint.y += vektorPravca[1] * s * firstPoint.w;
+        firstPoint.z += vektorPravca[2] * s * firstPoint.w;
+        // vecAdd(this.pos,id1, this.grads,0, -s * w1);
+        secondPoint.x -= vektorPravca[0] * s * secondPoint.w;
+        secondPoint.y -= vektorPravca[1] * s * secondPoint.w;
+        secondPoint.z -= vektorPravca[2] * s * secondPoint.w;
+
+        if(i == loggingIndex)
+            console.log(bendingEdges[i].first.x, bendingEdges[i].first.y, bendingEdges[i].first.z, bendingEdges[i].first.velocity);
+    }
+};
+
+function postSolve(dt) 
+{
+    for(let i=0; i < numberOfPoints; ++i)
+    {
+        for(let j=0; j < numberOfPoints; ++j)
+        {
+            if(points[i][j].w == 0) continue;
+            // vecSetDiff(this.vel,i, this.pos,i, this.prevPos,i, 1.0 / dt);
+            points[i][j].velocity.x = (points[i][j].x - points[i][j].previous[0]) / dt;
+            points[i][j].velocity.y = (points[i][j].y - points[i][j].previous[1]) / dt;
+            points[i][j].velocity.z = (points[i][j].z - points[i][j].previous[2]) / dt;
+        }
+    }
+};
+
+function animate(timestamp)
+{
+    if(paused) return;
+    currentTime = timestamp;
+    deltaTime = currentTime - previousTime;
+  
+    if (deltaTime > dt) 
+    {
+        previousTime = currentTime - (deltaTime % dt);
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // rotateX();
+        // rotateY();
+        // rotateZ();
+        xpbd();
+        for(const row of points)
+        {
+            for(const point of row)
+            {
+                point.draw(drawPoints);
+            }
+        }
+
+    }
+
+    // Dodatan info
+    if(showScreenCenter)
+    {
+        ctx.beginPath();
+        ctx.arc(window.innerWidth/2, window.innerHeight/2, 3, 0, Math.PI*2);
+        ctx.fill();
+    }
+
+    window.requestAnimationFrame(animate);
+}
+
+function play() 
+{
+    paused = !paused;
+    if(!paused) animate();
+}
+
+let direction = 1;
+function rotateX(deg=Math.PI/18) 
+{
+    for(const row of points)
+    {
+        for(const point of row)
+        {
+          point.rotateX(direction*deg);
+        }
+    }
+}
+
+function rotateY(deg=Math.PI/18) 
+{
+    for(const row of points)
+    {
+        for(const point of row)
+        {
+          point.rotateY(direction*deg);
+        }
+    }
+}
+
+function rotateZ(deg=Math.PI/18) 
+{
+    for(const row of points)
+    {
+        for(const point of row)
+        {
+          point.rotateZ(direction*deg);
+        }
+    }
+}
+
+function changeDirection() 
+{
+    direction *= -1;
+}
+
+const controlMapping = [play, () => rotateX(), () => rotateY(), () => rotateZ(), changeDirection];
+const controlsDiv = document.getElementById("controls");
+
+for(let i=0; i<controlsDiv.children.length; ++i)
+{
+    controlsDiv.children[i].addEventListener("click", controlMapping[i]);
+}
+
+console.log(stretchingEdges.length);
+console.log(bendingEdges.length);
+
+animate();
